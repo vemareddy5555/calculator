a11b2152f20a3fd55d2a2cf29032662ad602b7b4
vemareddy5555 <k.vemareddy20@gmail.com>
2020-11-02 17:42:19 +0530

    Modifaction 2

--- function.py
+++ function.py
@@ -1,11 +1,26 @@
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+   return x/y
+
 x = int(input('Enter x value: '))
 y = int(input('Enter y value: '))
 f = input('Enter function: ')
-if f = 'add':
+if f == 'add':
     print(add(x,y))
-elif f = 'sub':
+elif f == 'sub':
     print(sub(x,y))
-elif f = 'mul':
+elif f == 'mul':
     print(mul(x,y))
-elif f = 'div':
+elif f == 'div':
     print(div(x,y))
