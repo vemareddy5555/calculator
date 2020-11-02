c7acd3ec9908dadd4b11b924ef8942dce3ee8537
vemareddy5555 <k.vemareddy20@gmail.com>
2020-11-02 15:53:44 +0530

    Modified function

---
+++ function.py
@@ -0,0 +1,11 @@
+x = int(input('Enter x value: '))
+y = int(input('Enter y value: '))
+f = input('Enter function: ')
+if f = 'add':
+    print(add(x,y))
+elif f = 'sub':
+    print(sub(x,y))
+elif f = 'mul':
+    print(mul(x,y))
+elif f = 'div':
+    print(div(x,y))
